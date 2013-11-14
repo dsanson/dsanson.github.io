224cef7a3a055321bc05572926f119f1997125ed
David Sanson <dsanson@gmail.com>
2013-11-14 15:07:22 -0600

    changing to sass; local image; sunil

---
+++ sunil/README.txt
@@ -0,0 +1,18 @@
+To deploy, copy the following files to a web accessible folder:
+
+-   advisors.html
+-   lookup.js
+-   data.json
+
+You must generate the file, 'data.json' from your Excel spreadsheet. To do this, 
+
+1.  Create a spreadsheet that contains just two columns, with the headers "ulid" and "advisor".
+
+2.  Save this spreadsheet as a CSV file.
+
+3.  Convert this CSV file to a JSON file, named 'data.json'.
+
+For step (3), I used this website:
+
+-   <http://www.cparker15.com/code/utilities/csv-to-json/>
+
---
+++ sunil/lookup.js
@@ -0,0 +1,28 @@
+var ulid = getQueryVariable("ulid");
+
+if (typeof ulid == "string") {
+$.getJSON("data.json",
+        function(data){
+            var match
+            for (var i = 0; i < data.length; i++) {
+                if (data[i]["ulid"] == ulid) {
+                    match = 1;
+                    $("#report").text(ulid + "'s advisor is " + data[i]["advisor"]);
+                }
+
+            }
+            if (match != 1) {
+                    $("#report").text(ulid + " cannot be found.");
+                }
+        });
+
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  } 
+}}
