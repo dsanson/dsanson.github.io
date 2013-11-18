6d4f68f3ffbead788cd07b6f4e751f27321e6242
David Sanson <dsanson@gmail.com>
2013-11-18 11:34:02 -0600

    fixing error in sunil/lookup.js

--- sunil/lookup.js
+++ sunil/lookup.js
@@ -1,3 +1,14 @@
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  } 
+}
+
 var ulid = getQueryVariable("ulid");
 
 if (typeof ulid == "string") {
@@ -15,14 +26,4 @@ $.getJSON("data.json",
                     $("#report").text(ulid + " cannot be found.");
                 }
         });
-
-function getQueryVariable(variable) {
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-    if (pair[0] == variable) {
-      return pair[1];
-    }
-  } 
-}}
+}
