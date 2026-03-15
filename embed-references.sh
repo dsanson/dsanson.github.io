9c8cbb61841914d4a889d8f7d4477be2ea5ba9b0
David Sanson <dsanson@gmail.com>
2026-03-15 15:45:19 -0500

    fixing error in embed-references.sh

--- embed-references.sh
+++ embed-references.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 cp "$1" "$1.bk"
-pandoc "$1" --citeproc --lua-filter add-citations-to-metadata.lua --bibliography /Users/desanso/d/research/zotero.json -t markdown -s -o "$1"
+pandoc "$1" --lua-filter add-citations-to-metadata.lua --bibliography /Users/desanso/d/research/zotero.json -t markdown -s -o "$1"
